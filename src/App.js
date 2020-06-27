ffe655cba8bba47a130417f6068c6b2cec1689e7
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-27 19:07:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Github from './Github';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Github />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Github.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Github extends Component{
+    render(){
+        return (
+        <div>Hello from Github</div>
+        )
+    }
+}
+
+export default Github;
